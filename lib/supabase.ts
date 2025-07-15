5c4b921d62a482057182fee1bc14ebbf8e11a49c
esc4n0rx <paulo.cunha@hortifruti.com.br>
2025-07-15 17:58:16 -0300

    Remove logs de configuração do Supabase no arquivo supabase.ts, simplificando o código e melhorando a segurança ao evitar a exposição de chaves sensíveis.

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -4,11 +4,6 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 const supabaseKey = process.env.NEXT_PUBLIC_SERVICE_ROLE_KEY!
 
-console.log(supabaseUrl)
-console.log(supabaseAnonKey)
-console.log(supabaseKey)
-
-// Cliente para uso no browser (client-side)
 export const supabaseClient = createClient(supabaseUrl, supabaseAnonKey)
 
 export const supabaseServer = createClient(supabaseUrl, supabaseKey)
